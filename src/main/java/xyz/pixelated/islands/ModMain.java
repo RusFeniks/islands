218a0ea006094c7ef341ca28c13d46313f4b37a3
Wynd <wyndmaster@gmail.com>
2020-07-21 09:11:57 +0300

    Used ATs for a server properties and now islands can spawn on servers

--- src/main/java/xyz/pixelated/islands/ModMain.java
+++ src/main/java/xyz/pixelated/islands/ModMain.java
@@ -1,7 +1,10 @@
 package xyz.pixelated.islands;
 
+import net.minecraft.server.dedicated.ServerProperties;
 import net.minecraft.world.WorldType;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import xyz.pixelated.islands.config.CommonConfig;
 import xyz.pixelated.islands.init.ModValues;
 
@@ -13,5 +16,18 @@ public class ModMain
 	public ModMain()
 	{
 		CommonConfig.init();
+		
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::dedicatedServerSetup);
+	}
+	
+	public void dedicatedServerSetup(FMLDedicatedServerSetupEvent event)
+	{
+		ServerProperties serverProperties = event.getServerSupplier().get().getServerProperties();
+		if (serverProperties != null)
+		{
+			String entryValue = serverProperties.serverProperties.getProperty("use-modded-worldtype");
+			if (entryValue != null && entryValue.equals("islands"))
+				serverProperties.worldType = ISLANDS_TYPE;
+		}
 	}
 }

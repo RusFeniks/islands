e33eb80c8e0d9bbca7b02e2eadc41617b7ee8669
Tokeli <6316563+Tokeli@users.noreply.github.com>
2020-08-16 03:29:23 -0400

    Enforce overworld-only
    Change IslandWorldType to modify only the overworld.

--- src/main/java/xyz/pixelated/islands/IslandWorldType.java
+++ src/main/java/xyz/pixelated/islands/IslandWorldType.java
@@ -2,6 +2,7 @@ package xyz.pixelated.islands;
 
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
+import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.ChunkGeneratorType;
 import net.minecraft.world.gen.OverworldGenSettings;
@@ -16,6 +17,9 @@ public class IslandWorldType extends WorldType
 	@Override
 	public ChunkGenerator<?> createChunkGenerator(World world)
 	{
-		return ChunkGeneratorType.SURFACE.create(world, new IslandBiomeProvider(world.getSeed()), new OverworldGenSettings());
+		if(world.getDimension().getType() == DimensionType.OVERWORLD)
+		    return ChunkGeneratorType.SURFACE.create(world, new IslandBiomeProvider(world.getSeed()), new OverworldGenSettings());
+
+		return super.createChunkGenerator(world);
 	}
 }

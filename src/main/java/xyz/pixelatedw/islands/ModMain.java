a6aee016259c01a253755ae36b2cb86b63ffcb35
Wynd <wyndmaster@gmail.com>
2020-11-10 23:15:57 +0200

    Turned ModMain into an event subscriber

--- src/main/java/xyz/pixelatedw/islands/ModMain.java
+++ src/main/java/xyz/pixelatedw/islands/ModMain.java
@@ -2,25 +2,25 @@ package xyz.pixelatedw.islands;
 
 import net.minecraft.server.dedicated.ServerProperties;
 import net.minecraft.world.WorldType;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import xyz.pixelatedw.islands.config.CommonConfig;
 import xyz.pixelatedw.islands.init.ModValues;
 
 @Mod(ModValues.PROJECT_ID)
+@Mod.EventBusSubscriber(modid = ModValues.PROJECT_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModMain
 {
 	public static final WorldType ISLANDS_TYPE = new IslandWorldType();
 
 	public ModMain()
 	{
-		CommonConfig.init();
-		
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::dedicatedServerSetup);
+		CommonConfig.init();		
 	}
 	
-	public void dedicatedServerSetup(FMLDedicatedServerSetupEvent event)
+	@SubscribeEvent
+	public static void dedicatedServerSetup(FMLDedicatedServerSetupEvent event)
 	{
 		ServerProperties serverProperties = event.getServerSupplier().get().getServerProperties();
 		if (serverProperties != null)
